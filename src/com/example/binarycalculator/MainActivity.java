d5313a1832f36db71bdf164518a1833cd81cea90
Radu Cracut <raducracut@hotmail.com>
2013-11-09 16:51:44 -0800

    Binary Calculator

---
+++ src/com/example/binarycalculator/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.binarycalculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class MainActivity extends Activity {
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+
+	    // Set the user interface layout for this Activity
+	    // The layout file is defined in the project res/layout/main_activity.xml file
+	    setContentView(R.layout.activity_main);
+	   
+	    
+		}
+}
